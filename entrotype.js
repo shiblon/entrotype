88474ff616029af9ee44eaf98b904c81de223779
Chris Monson <shiblon@gmail.com>
2012-08-25 22:17:07 +0200

    Rearranged a few things, added some level definitions, and cleaned up some oopses.  We now have the ability to do a basic game with basic levels.

--- entrotype.js
+++ entrotype.js
@@ -1,274 +1,62 @@
-// Requires jquery.
+BasicGame = function(keyboardLayout) {
+  // Difficulty, easy to hard.
+  // The contents of this array are, for each difficulty level, a Keyboard
+  // layout query that corresponds to it.
+  //
+  // Each level can be summarized by taking the union of the elements.
+  // Each group can be summarized, then at each level, summarize all the
+  // previous levels.
+  //
+  // At the end, we should have them all in the summary.
+  this.keyboardLayout = keyboardLayout;
+  // Note that "S0" is a query that has to be used implicitly, pretty much all
+  // the time.  It is treated specially.
+  this._level_queries = [
+      ["Home Row",    ["H2-", "H3-", "H4-", "H56-", "H1-"]],
+      ["Top Row",     ["T2-", "T3-", "T4-", "T5-", "T1-", "T6-"]],
+      ["Bottom Row",  ["B2-", "B3-", "B4-", "B5-", "B1-", "B6-"]],
+      ["Numbers Row", ["N2-", "N3-", "N4-", "N5-", "N1-", "N6-"]],
+      ["Shift Key",   ["H123456!", "T123456!", "B123456!", "N123456!"]],
+  ];
 
-$(document).ready(function(){
-  // This is a simplified keyboard map using the approach described in
-  // KeyboardLayout constructor.
-  ANSI_QWERTY = [
-      ["`~", "NL6"],
-      ["1!", "NL5"],
-      ["2@", "NL4"],
-      ["3#", "NL3"],
-      ["4$", "NL2"],
-      ["5%", "NL1"],
-      ["6^", "NR1"],
-      ["7&", "NR2"],
-      ["8*", "NR3"],
-      ["9(", "NR4"],
-      ["0)", "NR5"],
-      ["-_", "NR6"],
-      ["=+", "NR6"],
-      ["qQ", "TL5"],
-      ["wW", "TL4"],
-      ["eE", "TL3"],
-      ["rR", "TL2"],
-      ["tT", "TL1"],
-      ["yY", "TR1"],
-      ["uU", "TR2"],
-      ["iI", "TR3"],
-      ["oO", "TR4"],
-      ["pP", "TR5"],
-      ["[{", "TR6"],
-      ["]}", "TR6"],
-      ["\|", "TR6"],
-      ["aA", "HL5"],
-      ["sS", "HL4"],
-      ["dD", "HL3"],
-      ["fF", "HL2"],
-      ["gG", "HL1"],
-      ["hH", "HR1"],
-      ["jJ", "HR2"],
-      ["kK", "HR3"],
-      ["lL", "HR4"],
-      [";:", "HR5"],
-      ["'\"", "HR6"],
-      ["zZ", "BL5"],
-      ["xX", "BL4"],
-      ["cC", "BL3"],
-      ["vV", "BL2"],
-      ["bB", "BL1"],
-      ["nN", "BR1"],
-      ["mM", "BR2"],
-      [",<", "BR3"],
-      [".>", "BR4"],
-      ["/?", "BR5"],
-      [" ", "SLR0"]];
+  // Levels: For each title and group:
+  //  - Title is "Level " + (query_index + 1)
+  //  - Then there's a level summary that is the union of the group.
+  //  - Then there's a level that's the summary of the previous summaries.
+  //  - TODO: We need to treat S0 specially, adding it in for particular
+  //          kinds of play. It is not always appropriate to use it, so
+  //          it is not included in the level definitions directly.
+  this._levels = [];
+  var current_summary = "";
+  for (var qgi = 0, len = this._level_queries.length; qgi < len; ++qgi) {
+    var qg = this._level_queries[qgi];
+    var qtitle = qg[0];
+    var qgroup = qg[1];
+    var summary = this.keyboardLayout.queryUnion(qgroup);
 
-  KeyboardLayout = function(simplifiedLayout) {
-    // Mapping from characters to finger and distance.
-    // Pair: <chars, position> where position has three elements.
-    //  For example, ["fF", "HL2-!"] means
-    //  "f and F are located on the Home row, Left hand, 2nd finger."
-    //  "-!" means the first character is unmodified, the second needs "Shift".
-    //
-    //  The second part of the pair is Row, Hand, Finger, and the values are
-    //  Row: N=Number, T=Top, H=Home, B=Bottom, S=Space
-    //  Hand: L=Left, R=Right
-    //  Finger: 0=Thumb, 1=Index Reach, 2=Index,
-    //          3=Middle, 4=Ring, 5=Small, 6=Small Reach
-    //          where "Reach" means the finger moves left or right of neutral.
-    //  Note that there is no overlap in these values, so you can also specify
-    //  alternates (e.g., the space bar can be used with either hand, so it can
-    //  have a mapping of "SLR0", meaning "space row, left, right, thumb").
-    //
-    //  Modifiers are defined as below:
-    //    -=Empty (no modifier)
-    //    !=Shift
-    //    @=Meta
-    //    ^=Control
-    //    #=Shift-Meta
-    //    $=Shift-Control
-    //    %=Meta-Control
-    //    &=Shift-Meta-Control
-    //  Modifiers must appear in order, and there must be the same number as
-    //  there are characters in the first part of the pair. They can be
-    //  anywhere in the string, but their relative order matters.
-    //
-    //  The one exception to this is when the modifiers would simple be "-!".
-    //  In this case the modifier string can simply be elided, so
-    //  ["fF", "HL2"] is the same as ["fF", "HL2-!"].
-    //
-
-    // These are all of our values for each class (row, hand, finger, mod).
-    this.CLASS_ROW = "SBHTN";
-    this.CLASS_HAND = "LR";
-    this.CLASS_FINGER = "0123456";
-    this.CLASS_MODIFIER = "-!@^#$%&";
-
-    var strToSet = function(s) {
-      var d = {};
-      for (var i = 0; i < s.length; ++i) {
-        d[s[i]] = true;
-      }
-      return d;
+    for (qi in qgroup) {
+      var title = qtitle + " " + (Number(qi) + 1);
+      this._levels.push({title: title, query: qgroup[qi]});
     }
-
-    this._row_dict = strToSet(this.CLASS_ROW);
-    this._hand_dict = strToSet(this.CLASS_HAND);
-    this._finger_dict = strToSet(this.CLASS_FINGER);
-    this._modifier_dict = strToSet(this.CLASS_MODIFIER);
-
-    // Now define a more complete (and verbose) layout from the simplified spec.
-    // This is simply a dictionary, keyed on the character, whose value is the
-    // complete map spec for that character, split into classes, in order of
-    // row, hand, finger, and modifier.
-    //
-    // e.g., "f": ["H", "L", "2", "-"]
-    this._layout = {};
-    for (sli in simplifiedLayout) {
-      var spec = simplifiedLayout[sli];
-      var chars = spec[0];
-      var classes = spec[1];
-
-      // First find the modifiers, if any.  If none are specified, we use "-"
-      // or "-!", depending on how many characters there are.  It is an error
-      // to not specify modifiers for 3 or more characters.
-      var split_spec = this.splitSpecStr(classes);
-      if (split_spec.modifiers.length == 0) {
-        if (chars.length == 1) {
-          split_spec.modifiers = "-";
-        } else if (chars.length == 2) {
-          split_spec.modifiers = "-!";
-        }
-      }
-
-      if (split_spec.modifiers.length != chars.length) {
-        throw ("Invalid modifiers '" + split_spec.modiiers +
-               "' for char spec '" + chars + "'");
-      }
-
-      char_mod_pairs = [];
-      for (var i = 0; i < chars.length; ++i) {
-        // Create a string with two characters: char and modifier
-        char_mod_pairs.push(chars[i] + split_spec.modifiers[i]);
-      }
-
-      for (cmi in char_mod_pairs) {
-        var cm = char_mod_pairs[cmi];
-        var c = cm[0];
-        var m = cm[1];
-        if (this._layout[c] != undefined) {
-          throw "Repeated character in layout: '" + c + "'";
-        }
-        this._layout[c] = [split_spec.rows,
-                           split_spec.hands,
-                           split_spec.fingers,
-                           m];
-      }
-    }
-  };
-
-  KeyboardLayout.prototype.splitSpecStr = function(specStr) {
-    var split = {rows: "", hands: "", fingers: "", modifiers: ""};
-    for (ci in specStr) {
-      var c = specStr[ci];
-      if (this._row_dict[c]) {
-        split.rows += c;
-      } else if (this._hand_dict[c]) {
-        split.hands += c;
-      } else if (this._finger_dict[c]) {
-        split.fingers += c;
-      } else if (this._modifier_dict[c]) {
-        split.modifiers += c;
+    this._levels.push({title: qtitle + " - Review", query: summary});
+    current_summary = this.keyboardLayout.queryUnion(
+        [current_summary, summary]);
+    var review_title = "";
+    if (qgi > 0) {
+      if (qgi < (len-1)) {
+        review_title = "Cumulative Review " + (Number(qgi) + 1);
       } else {
-        console.error("Unknown mapping spec: " + c);
-        return null;
+        review_title = "Final Review";
       }
+    this._levels.push({title: review_title, query: current_summary});
     }
-    return split;
-  };
+  }
 
-  KeyboardLayout.prototype.nonEmptyIntersection = function(arr1, arr2) {
-    for (a1 in arr1) {
-      for (a2 in arr2) {
-        if (arr1[a1] == arr2[a2]) {
-          return true;
-        }
-      }
-    }
-  };
+  this.curChar = null;
+};
 
-  // Query the keyboard layout for all keys that fit a set of specified
-  // constraints. Different constraint classes are ANDed, and within a class
-  // values are ORed. For example, to specify all characters on the home,
-  // bottom, or top rows, you could say "HBT".  To specify only those
-  // on the home row that are in easy reach, you would specify
-  // "H2345". To specify only the left hand in that case, add "L": "HL2345".
-  // Any constraint class that is not mentioned is assumed to be "don't care".
-  //
-  // Modifiers are handled differently (since they aren't specified in the
-  // mapping explicitly).  They are assigned the following values:
-  // C=Ctrl
-  // M=Meta
-  // U=shift (Upper)
-  //
-  // NOTE: there is currently no way to specify that you *must* combine
-  // two meta keys (e.g., for Ctrl-Shift combinations).
-  KeyboardLayout.prototype.query = function(constraintStr) {
-    var split_spec = this.splitSpecStr(constraintStr);
-
-    // Search for all keys that satisfy these constraints.  That means that
-    // all specified query classes have a matching element in each key.
-    var matching_chars = [];
-
-    for (c in this._layout) {
-      var spec = this._layout[c];
-      var rows = spec[0];
-      var hands = spec[1];
-      var fingers = spec[2];
-      var modifiers = spec[3];
-
-      if ((split_spec.rows.length == 0 ||
-           this.nonEmptyIntersection(split_spec.rows, rows)) &&
-          (split_spec.hands.length == 0 ||
-           this.nonEmptyIntersection(split_spec.hands, hands)) &&
-          (split_spec.fingers.length == 0 ||
-           this.nonEmptyIntersection(split_spec.fingers, fingers)) &&
-          (split_spec.modifiers.length == 0 ||
-           this.nonEmptyIntersection(split_spec.modifiers, modifiers))) {
-        matching_chars.push(c);
-      }
-    }
-    return matching_chars;
-  };
-
-  Game = function() {
-    this.running = false;
-    this.curChar = null;
-  };
-
-  Game.prototype.showRandomChar = function(container) {
-    var charnum = Math.floor(Math.random() * 256);
-    console.log(String.fromCharCode(charnum));
-  };
-
-  // Global information.
-  ansi_layout = new KeyboardLayout(ANSI_QWERTY);
-  game = new Game();
-
-  // Key Up
-  $(document).keypress(function(event){
-    console.log("key: " + event.which);
-    event.preventDefault();
-  });
-
-  // Initial setup
-  $("#btn_start").
-    val("Start").
-    click(function(event){
-      var target = event.delegateTarget;
-      console.log("Clicked button " + target.value);
-      game.running = !game.running;
-      target.value = (game.running) ? "Stop" : "Start";
-    });
-
-  $("#div_viewport").text("Click 'Start' to begin.");
-
-  // Starting a game:
-  // - Show a countdown for 3 seconds
-  // - Show a key to strike, listen for key events
-  // - Set a timer for a maximum delay for a press to occur
-  //    - Key pressed: show happiness (or sadness) then next character
-  //    - Timer fired: show sadness then next character
-  // - Always set a new timer with a new character
-  // - Go until stopped.
-});
+BasicGame.prototype.showRandomChar = function(level) {
+  var possible_chars = this.keyboardLayout.query(level.query);
+  var c = possible_chars[Math.floor(Math.random() * possible_chars.length)];
+  console.log(c);
+};
