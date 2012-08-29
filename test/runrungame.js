69d891912e6cdd466cc6b383d95aa36d06b93f2f
Chris Monson <shiblon@gmail.com>
2012-08-29 22:21:39 +0200

    More random changes to test an animation loop idea.

--- test/runrungame.js
+++ test/runrungame.js
@@ -5,11 +5,14 @@ RunRunGame = function(parentContainer, randomCharFunc, options) {
 
   this._parent = $(parentContainer);
   this._char_func = randomCharFunc;
+  this._running = false;
 
-  this.init();
+  this._tickers = [];
+
+  this.reset();
 };
 
-RunRunGame.prototype.init = function() {
+RunRunGame.prototype.reset = function() {
   this._parent.empty();
   this._svg = this._parent.svg({onLoad: $.proxy(this.drawIntro, this)});
 };
@@ -19,4 +22,36 @@ RunRunGame.prototype.drawIntro = function(svg) {
       {fill: "none", stroke: "red", strokeWidth: "3"});
   console.log(circle);
   setTimeout(function() { circle.setAttribute("cx", 200); }, 2000);
+};
+
+RunRunGame.prototype.addTicker = function(ticker) {
+  this._tickers.push(ticker);
+};
+
+RunRunGame.prototype._start = function() {
+  if (this._running) {
+    return;
+  }
+  $.each(this._tickers, function(i, ticker) {
+    ticker.reset();
+  });
+  this._running = true;
+  this._start_time = new Date().getTime();
+  this._interval_timer = setInterval($.proxy(this._tick, this), 50);
+};
+
+RunRunGame.prototype._tick = function() {
+  var elapsed = new Date().getTime() - this._start_time;
+  $.each(this._tickers, function(i, ticker) {
+    ticker.tick(this, this._start_time, elapsed);
+  });
+};
+
+RunRunGame.prototype._stop = function() {
+  if (!this._running) {
+    return;
+  }
+  this._running = false;
+  clearInterval(this._interval_timer);
+  this._interval_timer = null;
 };
